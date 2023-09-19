7e5b1a938990332fd4fe9a3fe5300f3daaf467c5
Felipe Castilhos <felipecastilhos@gmail.com>
2023-09-18 21:20:15 -0300

    feat: move the bird up when the space button is pressed

---
+++ bird.gd
@@ -0,0 +1,8 @@
+extends RigidBody2D
+
+func _ready() -> void:
+	$AnimatedSprite2D.play()
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed('Flap'):
+		linear_velocity = Vector2(0, -450)
