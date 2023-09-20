0a15f260e3be1824f6e3312634b912fa35992fb6
Felipe Castilhos <felipecastilhos@gmail.com>
2023-09-20 08:41:55 -0300

    feat: player dies after hitting a pipe

--- Scenes/bird.gd
+++ Scenes/bird.gd
@@ -1,8 +1,15 @@
 extends RigidBody2D
 
+var is_dead: bool = false
+
 func _ready() -> void:
 	$AnimatedSprite2D.play()
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed('Flap'):
-		linear_velocity = Vector2(0, -350)
+	if is_dead:
+		linear_velocity = Vector2(0, 350)
+	else:
+		if Input.is_action_pressed('Flap'):
+			linear_velocity = Vector2(0, -350)
+	
+	collision_layer = 0 if is_dead else 1
