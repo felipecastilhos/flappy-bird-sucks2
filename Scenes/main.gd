36cf13a2164b1351d62a2a34ff2847c5d07c6750
Felipe Castilhos <felipecastilhos@gmail.com>
2023-09-20 20:41:45 -0300

    feat: detect game over state

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,7 +1,23 @@
-extends Node2D
+extends Node
 
 var PipesScene = preload("res://Scenes/pipes.tscn")
 
+enum GAME_STATE { PLAYING, STOP }
+var state: GAME_STATE = GAME_STATE.PLAYING
+
+func spawn_pipes():
+	var pipes = PipesScene.instantiate()
+	add_child(pipes)
+	pipes.hit_the_player.connect(_on_pipes_hit_the_player)
+		
+func game_over():
+	state = GAME_STATE.STOP
+	$PipeSpawner.stop()
+	print_debug("game over")
+
 func _on_pipe_spawner_timeout():
-	add_child(PipesScene.instantiate())
+	spawn_pipes()
+
+func _on_pipes_hit_the_player(): 
+	game_over()
 	
