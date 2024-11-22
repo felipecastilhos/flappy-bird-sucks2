46bfbeccfdb5ef050d738a543790801b089dbdd3
casti <felipecastilhos@gmail.com>
2024-11-22 19:50:05 -0300

    fix: hud not displaying on game over

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -4,23 +4,26 @@ signal game_was_over
 
 var PipesScene = preload("res://Scenes/pipes.tscn")
 
-@onready var parallaxBackground:ParallaxBackground = $ParallaxBackground
-@onready var hud: CanvasLayer = $HUD
-@onready var foregroundLayer: ParallaxLayer = $ParallaxForeground/ForegroundLayer
-@onready var pipeSpawner: Timer = $PipeSpawner
+@onready var pipe_spawner: Timer = $PipeSpawner
+@onready var game_boundaries: CollisionShape2D = $GameBoundaries/UpperLimitsShape2D3dd
+@onready var foreground_layer: ParallaxLayer = $ParallaxForeground/ForegroundLayer
 
+##### BACKGROUND #####
+@onready var parallax_background:ParallaxBackground = $ParallaxBackground
+@onready var backgroundLayer2: ParallaxLayer = parallax_background.get_node('BackgroundLayer2')
+@onready var backgroundLayer3: ParallaxLayer = parallax_background.get_node('BackgroundLayer3')
+@onready var backgroundLayer4: ParallaxLayer = parallax_background.get_node('BackgroundLayer4')
+
+##### HUD ZONE #####
+@onready var hud: CanvasLayer = $HUD
 @onready var messageLabel: Label = hud.get_node('MessageLabel')
 @onready var scoreCountLabel: Label = hud.get_node('ScoreCountLabel')
 @onready var startButton: Button = hud.get_node('StartButton')
-@onready var backgroundLayer2: ParallaxLayer = parallaxBackground.get_node('BackgroundLayer2')
-@onready var backgroundLayer3: ParallaxLayer = parallaxBackground.get_node('BackgroundLayer3')
-@onready var backgroundLayer4: ParallaxLayer = parallaxBackground.get_node('BackgroundLayer4')
-
 
 enum GAME_STATE { PLAYING, STOP }
 @export_category('Game Configs')
 @export var state: GAME_STATE = GAME_STATE.PLAYING
-@export var score: int = 0
+@export var score: float = 0
 @export var scoreIncrement: int = 1
 @export var gameDificultyMultiplier: float = 1
 
@@ -34,45 +37,46 @@ enum GAME_STATE { PLAYING, STOP }
 func _process(delta: float):
 	scoreCountLabel.text = str(score)
 	if state == GAME_STATE.PLAYING: 
-		move_parallax_background(delta)
+		_move_parallax_background(delta)
 
-func move_parallax_background(delta: float):
+func _move_parallax_background(delta: float):
 		backgroundLayer2.motion_offset.x += delta * backgroundParallaxSpeed1
 		backgroundLayer3.motion_offset.x -= delta * backgroundParallaxSpeed2
 		backgroundLayer4.motion_offset.x -= delta * backgroundParallaxSpeed3
-		foregroundLayer.motion_offset.x -= delta * backgroundParallaxSpeed4
+		foreground_layer.motion_offset.x -= delta * backgroundParallaxSpeed4
 
-func spawn_pipes():
+func _spawn_pipes():
 	var pipes = PipesScene.instantiate()
-	pipes.speed += score * gameDificultyMultiplier
-	pipes.bottomPipeMaxLimit += score * gameDificultyMultiplier
-	add_child(pipes)
 	pipes.player_was_hit.connect(_on_pipes_player_was_hit)
 	pipes.score_area_was_reached.connect(_on_pipes_score_area_reached)
 	game_was_over.connect(pipes.on_game_was_over)
+
+	pipes.speed += score * gameDificultyMultiplier
+	pipes.bottomPipeMaxLimit += score * gameDificultyMultiplier
+	add_child(pipes)
 	
-func score_point():
+func _score_point():
 	score += scoreIncrement
-	if score % 10 == 0:
+	if fmod(score, 10) == 0:
 		gameDificultyMultiplier += 0.1
 	
-func game_over():
+func _game_over():
 	state = GAME_STATE.STOP
-	pipeSpawner.stop()
+	pipe_spawner.stop()
 	messageLabel.visible = true
 	startButton.visible = true
 	game_was_over.emit()
 	
 func _on_pipe_spawner_timeout():
 	var score_ratio = score / 2
-	pipeSpawner.wait_time -= 1 * score_ratio
-	spawn_pipes()
+	pipe_spawner.wait_time -= 1 * score_ratio
+	_spawn_pipes()
 
 func _on_pipes_player_was_hit():
-	game_over()
+	_game_over()
 	
 func _on_pipes_score_area_reached():
-	score_point()
+	_score_point()
 	
 func _on_start_button_pressed() -> void:
 	get_tree().reload_current_scene()
